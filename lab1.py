cbea1fbbac994984a2af1a3858477d28f993b293
muteb-dawood <78134579+muteb-dawood@users.noreply.github.com>
2021-02-03 15:36:12 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("Hello word!")
+
+
+# # python 
+
+# <p> This is my <b> first Python code </b>
+
+# https://www.taibahu.edu.sa/
+
+# [click this link]( https://www.taibahu.edu.sa)
+
+# [click this link]( https://www.taibahu.edu.sa “Taibah
+# University”)
+
+# $$ = \sqrt{a^2 + b^2}$$
+
+# $$e^{i\pi} + 1 = 0$$
+
+# $$e^x=\sum_{i=0}^\infty \frac{1}{i!}x^i$$
+
+# $$r'F(k) = \int_{-\infty}^{\infty} f{x} e^{2\pi i k} dx'$$
+
+# $$\begin {pmatrix}
+# 1 & a_1 & a_1^2 & \cdots & a_1^n \\
+# 1 & a_2 & a_2^2 & \cdots & a_2^n \\
+# \vdots & \vdots & \vdots & \ddots & \vdots \\
+# 1 & a_m & a_m^2 & \cdots & a_m^n
+# \end{pmatrix}$$
+# 
